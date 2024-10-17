de815951e51c75348cd4cb979b97c8f4f81f7dd7
Colibri Shin <colibrishin@gmail.com>
2024-10-18 01:05:00 +0900

    feat: add table query editor subsystem

---
+++ Source/wunthshin/Subsystem/TableQueryEditorSubsystem.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TableQueryEditorSubsystem.h"
+void UTableQueryEditorSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	// 자식 클래스에서 Object를 안넣었거나, 찾는 테이블이 없는 경우
+	ensure(DataTable);
+}
+
+FDataTableRowHandle UTableQueryEditorSubsystem::FindItem(const FName& InKey) const
+{
+	FDataTableRowHandle RowHandle;
+#if WITH_EDITOR
+	if (DataTable->GetRowMap().Find(InKey))
+	{
+		RowHandle.DataTable = DataTable;
+		RowHandle.RowName = InKey;
+	}
+	else
+	{
+		ensureAlwaysMsgf(false, TEXT("Unable to find row from table"));
+	}
+#else
+	if (ItemDataTable->GetRowMap().Find(InKey))
+	{
+		RowHandle.DataTable = ItemDataTable;
+		RowHandle.RowName = InKey;
+	}
+#endif // WITH_EDITOR
+
+	return RowHandle;
+}
