c161259b8d1d57a9b5658784e62f3f3f6a137b73
Colibri Shin <colibrishin@gmail.com>
2024-10-24 21:16:11 +0900

    feat: shield component

--- Source/wunthshin/Data/Elements/Crystalize/O_WSCrystalize.cpp
+++ Source/wunthshin/Data/Elements/Crystalize/O_WSCrystalize.cpp
@@ -3,6 +3,14 @@
 
 #include "O_WSCrystalize.h"
 
+#include "wunthshin/Components/Shield/C_WSShield.h"
+
 void UO_WSCrystalize::React(AActor* InReactionInstigator, AActor* InReactionAssist, AActor* InReactionOrigin)
 {
+	if (UC_WSShield* ShieldComponent = InReactionInstigator->GetComponentByClass<UC_WSShield>())
+	{
+		// todo: 쉴드 시간 조정 (레벨, 아이템 비례?)
+        ShieldComponent->SetShieldTime(5.f);
+        ShieldComponent->SetActive(true);
+	}
 }
