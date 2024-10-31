2596d797fafa67b9366657d7f4bbd9eea8bcf3c8
Colibri Shin <colibrishin@gmail.com>
2024-10-31 23:38:39 +0900

    feat: add common components
    공통의 컴포넌트들을 NPC 클래스에 추가

--- Source/wunthshin/Actors/Pawns/NPC/A_WSNPCPawn.cpp
+++ Source/wunthshin/Actors/Pawns/NPC/A_WSNPCPawn.cpp
@@ -2,15 +2,32 @@
 
 
 #include "wunthshin/Actors/Pawns/NPC/A_WSNPCPawn.h"
+
+#include "wunthshin/Components/Inventory/C_WSInventory.h"
+#include "wunthshin/Components/Shield/C_WSShield.h"
+#include "wunthshin/Components/Stats/StatsComponent.h"
+#include "wunthshin/Data/NPCs/NPCStats/NPCStats.h"
 #include "wunthshin/Data/NPCs/NPCTableRow/NPCTableRow.h"
+#include "wunthshin/Subsystem/Utility.h"
 #include "wunthshin/Subsystem/NPCSubsystem/NPCSubsystem.h"
 
 // Sets default values
 AA_WSNPCPawn::AA_WSNPCPawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	MeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	Inventory = CreateDefaultSubobject<UC_WSInventory>(TEXT("Inventory"));
+	Shield = CreateDefaultSubobject<UC_WSShield>(TEXT("Shield"));
+	StatsComponent = CreateDefaultSubobject<UStatsComponent>(TEXT("StatsComponent"));
+	RightHandWeapon = CreateDefaultSubobject<UChildActorComponent>(TEXT("RightHandWeapon"));
+}
+
+void AA_WSNPCPawn::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	FetchAsset(AssetName);
 }
 
 UScriptStruct* AA_WSNPCPawn::GetTableType() const
@@ -18,8 +35,35 @@ UScriptStruct* AA_WSNPCPawn::GetTableType() const
 	return FNPCTableRow::StaticStruct();
 }
 
-void AA_WSNPCPawn::ApplyAsset(const FDataTableRowHandle& InRowHandle)
+void AA_WSNPCPawn::ApplyAsset(const FTableRowBase* InRowPointer)
 {
+	if (!InRowPointer)
+	{
+		return;
+	}
+
+	const FNPCTableRow* TableRow = reinterpret_cast<const FNPCTableRow*>(InRowPointer);
+
+	if (TableRow->SkeletalMesh)
+	{
+		MeshComponent->SetSkeletalMesh(TableRow->SkeletalMesh);
+	}
+
+	if (TableRow->AnimInstance)
+	{
+		MeshComponent->SetAnimInstanceClass(TableRow->AnimInstance);
+	}
+
+	if (USubsystem* Subsystem = GetSubsystem())
+	{
+		const IDataTableQuery* TableQuery = Cast<IDataTableQuery>(Subsystem);
+		check(TableQuery);
+
+		if (const FNPCStats* Stats = TableQuery->GetRowValue<FNPCStats>(TableRow->Stats.RowName))
+		{
+			StatsComponent->InitializeStats(*Stats);
+		}
+	}
 }
 
 UClass* AA_WSNPCPawn::GetSubsystemType() const
@@ -39,13 +83,6 @@ void AA_WSNPCPawn::BeginPlay()
 {
 	Super::BeginPlay();
 	
-}
-
-// Called every frame
-void AA_WSNPCPawn::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
--- Source/wunthshin/Actors/Pawns/NPC/A_WSNPCPawn.h
+++ Source/wunthshin/Actors/Pawns/NPC/A_WSNPCPawn.h
@@ -9,18 +9,42 @@
 
 #include "A_WSNPCPawn.generated.h"
 
+class UStatsComponent;
+class UC_WSShield;
+class UC_WSInventory;
+
 UCLASS()
 class WUNTHSHIN_API AA_WSNPCPawn : public APawn, public IDataTableFetcher, public IElementTracked
 {
 	GENERATED_BODY()
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Mesh", meta=(AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* MeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Stats", meta = (AllowPrivateAccess = "true"))
+	UC_WSInventory* Inventory;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stats", meta = (AllowPrivateAccess = "true"))
+	UC_WSShield* Shield;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Stats", meta = (AllowPrivateAccess = "true"))
+	UStatsComponent* StatsComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
+	UChildActorComponent* RightHandWeapon;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Asset", meta = (AllowPrivateAccess = "true"))
+	FName AssetName;
+	
 public:
 	// Sets default values for this pawn's properties
 	AA_WSNPCPawn();
 
+	virtual void OnConstruction(const FTransform& Transform) override;
+
 	virtual UScriptStruct* GetTableType() const override;
 
-	virtual void ApplyAsset(const FDataTableRowHandle& InRowHandle) override;
+	virtual void ApplyAsset(const FTableRowBase* InRowPointer) override;
 
 	virtual UClass* GetSubsystemType() const override;
 #ifdef WITH_EDITOR
@@ -32,10 +56,10 @@ protected:
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	// 사용할 에셋의 이름
+	void SetAssetName(const FName& InAssetName) { AssetName = InAssetName; }
 
 };
