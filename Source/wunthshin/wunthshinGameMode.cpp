e08d949272234df2d8935a57ee40b06e76678a36
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-11-15 13:57:33 +0900

    fix: load character level snapshot before spawn 게임모드에서 플레이어의 폰을 생성하는 시점에서 캐릭터 서브시스템이 추적 및 생성한 플레이어를 반환함 만약 캐릭터가 초기 생성일 경우에는 서브시스템에 등록

--- Source/wunthshin/wunthshinGameMode.cpp
+++ Source/wunthshin/wunthshinGameMode.cpp
@@ -23,7 +23,8 @@ void AwunthshinGameMode::BeginPlay()
 APawn* AwunthshinGameMode::SpawnDefaultPawnAtTransform_Implementation(AController* NewPlayer, const FTransform& SpawnTransform)
 {
 	UCharacterSubsystem* CharacterSubsystem = GetGameInstance()->GetSubsystem<UCharacterSubsystem>();
-
+	CharacterSubsystem->LoadCharacterLevelSnapshot();
+	
 	// 만약 이전 레벨에 선택된 살아있는 캐릭터가 있었다면 해당 캐릭터를 반환
 	if (AA_WSCharacter* Character = CharacterSubsystem->GetCurrentCharacter())
 	{
@@ -48,5 +49,7 @@ APawn* AwunthshinGameMode::SpawnDefaultPawnAtTransform_Implementation(AControlle
 	// OnConstruction (에셋을 불러오는 시점) 전에 에셋을 지정
 	NewPawn->SetAssetName("YinLin");
 	NewPawn->FinishSpawning(SpawnTransform);
+	GetGameInstance()->GetSubsystem<UCharacterSubsystem>()->AddCharacter(NewPawn);
+	
 	return NewPawn;
 }
