366fa5f8301cc44f61c9a90c0ab281e1b62b4df8
Colibri Shin <colibrishin@gmail.com>
2025-03-25 17:46:23 +0900

    feat: DB Server continues (#97)

--- DBServer/DBServer.cpp
+++ DBServer/DBServer.cpp
@@ -1,20 +1,34 @@
-#include <iostream>
-#include <chrono>
+#include <mutex>
+#include <csignal>
+#include <condition_variable>
 #include "utility.hpp"
 #include "boost-socket.hpp"
 #include "message-handler.h"
 
+Network::NetworkContext G_TcpProtocol;
+MessageHandler          G_MessageHandler;
+
+void CleanUp( int signal )
+{
+    CONSOLE_OUT( __FUNCTION__, "Signal received, cleanup..." );
+    G_TcpProtocol.Destroy();
+}
+
 int main()
 {
     CONSOLE_OUT(__FUNCTION__, "Starting the Server");
-    Network::NetworkContext TcpProtocol;
-    MessageHandler          MessageHandler;
+    std::mutex              SleepLock;
+    std::unique_lock        Lock( SleepLock );
+    std::condition_variable SleepCondVar;
 
-    TcpProtocol.accept( std::bind( &MessageHandler::Handle,
-                                   &MessageHandler,
+    G_TcpProtocol.accept( std::bind( &MessageHandler::Handle,
+                                   &G_MessageHandler,
                                    std::placeholders::_1,
                                    std::placeholders::_2,
                                    std::placeholders::_3,
                                    std::placeholders::_4 ) );
 
+    std::signal( SIGINT | SIGTERM | SIGSEGV | SIGABRT | SIGFPE, CleanUp );
+
+    SleepCondVar.wait( Lock );
 }
--- DBServer/message-handler.cpp
+++ DBServer/message-handler.cpp
@@ -1,8 +1,29 @@
 #include "message-handler.h"
+#include "message.h"
 
 void MessageHandler::Handle( size_t                           index,
                              const boost::asio::mutable_buffer&     buffer,
                              const boost::system::error_code& ec,
                              const size_t                     read )
 {
+    if (read < 0)
+    {
+        // Invalid read
+        return;
+    }
+
+    MessageBase* message = reinterpret_cast<MessageBase*>( buffer.data() );
+    int32_t integerValue = static_cast<int32_t>( message->GetType() );
+    
+    if ( integerValue < 0 || integerValue >= GetMaxMessageIndex() )
+    {
+        // Unknown message
+        return;
+    }
+
+    if ( G_MessageSize.at(integerValue) != read )
+    {
+        // invalid size
+        return;
+    }
 }
